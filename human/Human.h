a27f9ffe73a5a109b668ff705fdbbf3dbaf30ab3
Artem Kosianich <maloyartem07@gmail.com>
2025-09-08 14:31:06 +0300

    так же я який все зробив

--- human/Human.h
+++ human/Human.h
@@ -1,5 +1,24 @@
 #pragma once
 class Human
 {
+private:
+	char* name;
+	char* surname;
+	int age;
+
+public:
+	Human();
+	Human(const char* n, const char* s, int a);
+	~Human();
+	void input(const char* n, const char* s, int a);
+	void print();
+
+	char* GetName();
+	char* GetSurname();
+	int GatAge();
+
+	void SetName(const char* n);
+	void SetSurname(const char* s);
+	void SetAge(int a);
 };
 
--- human/human.cpp
+++ human/human.cpp
@@ -8,4 +8,102 @@ int main()
     cout << "є котакбасіна  куда ты встал" << endl;
 }
 
+Human::Human()
+{
+    name = nullptr;
+    surname = nullptr;
+    age = 0;
+}
 
+Human::Human(const char* n, const char* s, int a)
+{
+    if (name != nullptr)
+    {
+		delete[] name;
+    }
+    if (surname != nullptr)
+	{   
+        delete[] surname;
+	}   
+    
+    name = new char[strlen(n) + 1];
+    strcpy(name, n);
+
+    surname = new char[strlen(s) + 1];
+    strcpy(surname, s);
+
+    age = a;
+
+}
+
+Human::~Human()
+{
+	delete[] name;
+	delete[] surname;
+}
+
+void Human::input(const char* n,const char* s,int a)
+{
+    char buf[100];
+    cout << "Name: ";
+    cin >> buf;
+    SetName(buf);
+
+    cout << "Surname: ";
+    cin >> buf;
+    SetSurname(buf);
+
+    cout << "Age: ";
+    int a;
+    cin >> a;
+    SetAge(a);
+}
+
+void Human::print()
+{
+	cout << "Name: " << GetName() << endl;
+	cout << "Surname: " << GetSurname() << endl;
+	cout << "Age: " << GatAge() << endl;
+}
+
+char* Human::GetName()
+{
+    return nullptr;
+}
+
+char* Human::GetSurname()
+{
+    return nullptr;
+}
+
+int Human::GatAge()
+{
+    return 0;
+}
+
+void Human::SetName(const char* n)
+{
+    if (name != nullptr)
+    {
+		delete[] name;
+    }
+	name = new char[strlen(n) + 1];
+	strcpy(name, n);
+}
+
+void Human::SetSurname(const char* s)
+{
+
+
+    if (surname != nullptr)
+    {
+		delete[] surname;
+    }
+	surname = new char[strlen(s) + 1];
+	strcpy(surname, s);
+}
+
+void Human::SetAge(int a)
+{
+	age = a;
+}
